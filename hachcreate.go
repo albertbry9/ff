9b9accf8141f4161c73ed34689e5c70575d8548a
albertbry9 <31410665+albertbry9@users.noreply.github.com>
2019-11-28 12:47:37 -0500

    Add files via upload

---
+++ hachcreate.go
@@ -0,0 +1,13 @@
+func init() {
+    rand.Seed(time.Now().UnixNano())
+}
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func RandStringRunes(n int) string {
+    b := make([]rune, n)
+    for i := range b {
+        b[i] = letterRunes[rand.Intn(len(letterRunes))]
+    }
+    return string(b)
+}
